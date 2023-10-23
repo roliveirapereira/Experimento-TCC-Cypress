bc7d813004c27ff4c95e6c22326bee4b0290356f
Lucca Borborema <lucca.santosborborema@pvai.com>
2023-10-23 17:08:14 -0300

    creating cypress structure

---
+++ cypress/tests/voluntario3/experimento.spec.js
@@ -0,0 +1,17 @@
+describe("Experimento voluntario", () => {
+  before(() => {
+    cy.visit("/");
+  });
+  context("Caso teste 1", () => {
+    it("Dynamic Loading", () => {});
+  });
+  context("Caso teste 2", () => {
+    it("Form Authentication", () => {});
+  });
+  context("Caso teste 3", () => {
+    it("Download File", () => {});
+  });
+  context("Caso teste 4", () => {
+    it("JavaScript Alerts", () => {});
+  });
+});
