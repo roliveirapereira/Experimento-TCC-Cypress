8d3190ef57b7bb0fb0ca23bd6f75b97a46d39768
Lucca Santos <64295041+luccaborborema@users.noreply.github.com>
2023-10-24 09:42:12 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://the-internet-herokuapp.com",
     specPattern: "cypress/tests/**/*.spec.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
