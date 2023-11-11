58deec517749b707640825d48e3307976d1496ee
Henrique Monteiro <hrqmonteiro@protonmail.com>
2023-11-11 14:24:52 -0300

    feat: adding context 1

--- cypress/tests/voluntario4/experimento.spec.js
+++ cypress/tests/voluntario4/experimento.spec.js
@@ -1,22 +1,34 @@
 describe("Experimento voluntario", () => {
   context("Caso teste 1", () => {
-    it("Dynamic Loading", () => {
+    it('Loads hidden element after clicking the "Start" button', () => {
       cy.visit("https://the-internet.herokuapp.com/");
+
+      cy.contains("Dynamic Loading").click();
+
+      cy.contains("Example 1: Element on page that is hidden").click();
+
+      cy.get("#start").should("be.visible");
+
+      cy.contains("Start").click();
+
+      cy.wait(5000);
+
+      cy.get("#finish").should("contain", "Hello World!");
     });
   });
-  context("Caso teste 2", () => {
-    it("Form Authentication", () => {
-      cy.visit("https://the-internet.herokuapp.com/");
-    });
-  });
-  context("Caso teste 3", () => {
-    it("Redirect Link", () => {
-      cy.visit("https://the-internet.herokuapp.com/");
-    });
-  });
-  context("Caso teste 4", () => {
-    it("JavaScript Alerts", () => {
-      cy.visit("https://the-internet.herokuapp.com/");
-    });
-  });
+  // context("Caso teste 2", () => {
+  //   it("Form Authentication", () => {
+  //     cy.visit("https://the-internet.herokuapp.com/");
+  //   });
+  // });
+  // context("Caso teste 3", () => {
+  //   it("Redirect Link", () => {
+  //     cy.visit("https://the-internet.herokuapp.com/");
+  //   });
+  // });
+  // context("Caso teste 4", () => {
+  //   it("JavaScript Alerts", () => {
+  //     cy.visit("https://the-internet.herokuapp.com/");
+  //   });
+  // });
 });
