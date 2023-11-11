baa9ce0f19fb96d836e6afadb0d099f7debee759
Henrique Monteiro <hrqmonteiro@protonmail.com>
2023-11-11 14:32:11 -0300

    feat: adding context 2

--- cypress/tests/voluntario4/experimento.spec.js
+++ cypress/tests/voluntario4/experimento.spec.js
@@ -16,11 +16,21 @@ describe("Experimento voluntario", () => {
       cy.get("#finish").should("contain", "Hello World!");
     });
   });
-  // context("Caso teste 2", () => {
-  //   it("Form Authentication", () => {
-  //     cy.visit("https://the-internet.herokuapp.com/");
-  //   });
-  // });
+  context("Caso teste 2", () => {
+    it.("Form Authentication", () => {
+      cy.visit("https://the-internet.herokuapp.com/");
+
+      cy.contains("Form Authentication").click();
+
+      cy.get("#username").type("tomsmith");
+
+      cy.get("#password").type("SuperSecretPassword!");
+
+      cy.get(".radius").click();
+
+      cy.contains("You logged into a secure area!");
+    });
+  });
   // context("Caso teste 3", () => {
   //   it("Redirect Link", () => {
   //     cy.visit("https://the-internet.herokuapp.com/");
